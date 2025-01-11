16895a48def3e0da7bece80573e0a835dcf7df00
Deepak Rizal <deepak2000rizal@gmail.com>
2025-01-11 22:22:03 +0530

    animation has been added to the dropdown

--- index.js
+++ index.js
@@ -2,13 +2,27 @@
 const dropDowns = document.querySelectorAll(".dropdown");
 
 dropDowns.forEach((dropdown) => {
-  dropdown.addEventListener("mouseenter", () => {
-    const content = dropdown.querySelector(".dropdown-content");
-    content.style.display = "block";
+  const link = dropdown.querySelector("a");
+  const content = dropdown.querySelector(".dropdown-content");
+
+  //show dropdown on hovering over the link
+  link.addEventListener("mouseenter", () => {
+    content.style.height = "500px";
+    content.style.opacity = "1";
+    content.style.pointerEvents = "auto";
+  });
+
+  content.addEventListener("mouseenter", () => {
+    content.style.height = "500px";
+    content.style.opacity = "1";
+    content.style.pointerEvents = "auto";
   });
 
   dropdown.addEventListener("mouseleave", () => {
-    const content = dropdown.querySelector(".dropdown-content");
-    content.style.display = "none";
+    content.style.opacity = "0";
+    setTimeout(() => {
+      content.style.height = "0";
+      content.style.pointerEvents = "none";
+    }, 200);
   });
 });
