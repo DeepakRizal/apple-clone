a2138aaeb5237b67b4a1e8e67891b86a4938eb6b
Deepak Rizal <deepak2000rizal@gmail.com>
2025-01-13 09:52:07 +0530

    fixed a bug

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-// Logic to handle dropdown functionality for larger screens
 const dropDowns = document.querySelectorAll(".dropdown");
 
 dropDowns.forEach((dropdown) => {
   const link = dropdown.querySelector("a");
   const content = dropdown.querySelector(".dropdown-content");
 
-  //show dropdown on hovering over the link
+  // Set initial styles to ensure consistency
+  content.style.height = "0";
+  content.style.opacity = "0";
+  content.style.pointerEvents = "none";
+  content.style.transition = "height 0.3s ease, opacity 0.2s ease";
+
+  // Show dropdown on hovering over the link
   link.addEventListener("mouseenter", () => {
     content.style.height = "500px";
     content.style.opacity = "1";
     content.style.pointerEvents = "auto";
   });
 
-  // hide the dropdown when the cursor leave the dropdown
-  content.addEventListener("mouseleave", (e) => {
+  // Hide the dropdown when the cursor leaves the dropdown
+  dropdown.addEventListener("mouseleave", () => {
     content.style.opacity = "0";
     setTimeout(() => {
       content.style.height = "0";
