b9fc1f6bc6cc21e404b4b0bddc50293c9adc84b2
Deepak Rizal <deepak2000rizal@gmail.com>
2025-01-13 09:59:29 +0530

    made a small refactor in the css

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ dropDowns.forEach((dropdown) => {
   const link = dropdown.querySelector("a");
   const content = dropdown.querySelector(".dropdown-content");
 
-  // Set initial styles to ensure consistency
-  content.style.height = "0";
-  content.style.opacity = "0";
-  content.style.pointerEvents = "none";
-  content.style.transition = "height 0.3s ease, opacity 0.2s ease";
-
   // Show dropdown on hovering over the link
   link.addEventListener("mouseenter", () => {
     content.style.height = "500px";
